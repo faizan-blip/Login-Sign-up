1c2a7214161e143259bb8a22cc94451fd83863dd
faizan-blip <faizanrock753@gmail.com>
2022-12-31 16:15:51 +0530

    edited

--- dashboard.js
+++ dashboard.js
@@ -10,11 +10,9 @@ function logout(){
     window.alert("Logging-Out");
 }
 const back = document.querySelector('.container');
-const h1 = document.querySelector('h1');
-const house = document.querySelector('i');
+
 
 function black(){
    back.classList.toggle('dark');
-   h1.classList.toggle('h1_dark');
-   house.classList.toggle('house');
+
 }
